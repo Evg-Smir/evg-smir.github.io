7c03185fc15cb9a00073a50e62b4c0df232185dc
Evgeniy-Smirnov <smrnovz@mail.ru>
2025-11-28 19:20:30 +0500

    Change app.vue for testing

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,8 @@
 <script setup lang="ts"></script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <h1>Hello my baby!</h1>
+  <img src="/image.jpg" alt="">
 </template>
 
 <style scoped></style>
