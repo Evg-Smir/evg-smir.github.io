8fa096e2f892b2f027eccfa73286ed0ddf6ae9d0
Evgeniy-Smirnov <smrnovz@mail.ru>
2025-11-29 11:51:21 +0500

    Try to get user info

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,20 @@
 <script setup lang="ts">
-import { retrieveLaunchParams } from '@tma.js/sdk';
-const { initDataRaw, initData } = retrieveLaunchParams();
+import { retrieveLaunchParams, retrieveRawInitData } from '@tma.js/sdk';
+const { initData } = retrieveLaunchParams();
+
+const initDataRaw = retrieveRawInitData()
+
+fetch('https://example.com/api', {
+  method: 'POST',
+  headers: {
+    Authorization: `tma ${initDataRaw}`
+  },
+});
 </script>
 
 <template>
   <div>
     <h1>Hello, World!</h1>
-    {{initDataRaw}}
-    {{ initData}}
   </div>
 </template>
 
